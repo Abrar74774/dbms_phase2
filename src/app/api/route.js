9b811f0bc868a00f646986d4a95913af7bca236c
Abrar74774 <abrarshahriarhossain@gmail.com>
2023-11-12 16:20:02 +0300

    Connect DB

---
+++ src/app/api/route.js
@@ -0,0 +1,16 @@
+import conn from "@/lib/db";
+
+export async function GET(req) {
+    let result;
+    try {
+        const query = 'SELECT * FROM persons;'
+        result = await conn.query(
+            query
+        );
+        console.log("ttt", result);
+    } catch (error) {
+        console.error("this is the error", error);
+    }
+
+    return Response.json({ rows: result.rows, fields: result.fields })
+}
